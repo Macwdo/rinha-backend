899659287bcb52ed6dff9973aaf10ce3a0b71ac1
Danilo Macedo <danilo.macedofernandes@hotmail.com>
2024-09-15 19:39:42 -0300

    adding docker compose with api and postgresql

--- src/main.py
+++ src/main.py
@@ -1,8 +1,46 @@
-from datetime import date, datetime
+from datetime import date
 from typing import Annotated
+import redis
+import asyncpg
+
 from fastapi import FastAPI, Response, status
 from pydantic import BaseModel, Field
 
+import os
+
+DATABASE = {
+    "host": os.environ.get("DB_HOST", "localhost"),
+    "port": os.environ.get("DB_PORT", 5432),
+    "user": os.environ.get("DB_USER", "postgres"),
+    "password": os.environ.get("DB_PASSWORD", "postgres"),
+    "database": os.environ.get("DB_NAME", "postgres"),
+}
+
+# `postgres://user:password@host:port/database?option=value
+DB_DSN = f"postgres://{DATABASE['user']}:{DATABASE['password']}@{DATABASE['host']}:{DATABASE['port']}/{DATABASE['database']}"
+
+
+class Database:
+    def __init__(self, dsn):
+        self._dsn = dsn
+
+    async def connect(self):
+        return await asyncpg.connect(dsn=self._dsn)
+
+
+class Cache:
+    def __init__(self, url):
+        self._redis = redis.Redis.from_url(url)
+
+    async def set(self, key, value):
+        self._redis.set(key, value)
+
+    async def get(self, key):
+        return self._redis.get(key)
+
+
+# cache = Cache(CACHE_URL)
+# db = Database(DB_DSN)
 
 app = FastAPI()
 
@@ -41,4 +79,5 @@ async def create_person(person: Person):
 
 @app.get("/contagem-pessoas")
 async def count_people():
-    pass
+    conn = await get_db()
+    return {"time": conn}
